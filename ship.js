6faa1c8128113b49a52b7726f6aa597e5dd9ae07
Emilowiz <Emil-josefsen023@hotmail.com>
2020-01-09 14:33:25 +0100

    iug

--- ship.js
+++ ship.js
@@ -1,33 +1,3 @@
-var ship;
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    ship = new Ship();
-}
-
-function draw() {
-    background(0);
-    ship.render();
-    ship.turn(0);
-    ship.update();
-    ship.edges();
-}
-
-function keyReleased() {
-    ship.setRotation(0);
-    ship.boosting(false);
-}
-
-function keyPressed() {
-    if (keyCode == RIGHT_ARROW) {
-        ship.setRotation(0.1);
-    } else if (keyCode == LEFT_ARROW) {
-        ship.setRotation(-0.1);
-    } else if (keyCode == UP_ARROW) {
-        ship.boosting(true);
-    }
-}
-
 function Ship() {
     this.pos = createVector(width / 2, height / 2)
     this.r = 20;
@@ -49,17 +19,19 @@ function Ship() {
     }
 
     this.boost = function() {
-        var force = p5.Vector.fromAngle(this.heading);
+        let force = p5.Vector.fromAngle(this.heading);
         force.mult(0.1);
         this.vel.add(force);
     }
 
     this.render = function() {
+        push();
         translate(this.pos.x, this.pos.y);
         rotate(this.heading + PI/2);
-        noFill();
+        fill(0);
         stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+        pop();
     }
 
     this.edges = function() {
