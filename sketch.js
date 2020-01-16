e564a0b41dd2a5ca5476dbfdf662e5d684511705
benjiiigreen <palle123g@gmail.com>
2020-01-16 13:52:45 +0100

    evw

--- sketch.js
+++ sketch.js
@@ -63,12 +63,5 @@ function keyPressed() {
         ship.setRotation(-0.1);
     } else if (keyCode == UP_ARROW) {
         ship.boosting(true);
-    } else if (key == 'd') {
-        ship.setRotation(0.1);
-    } else if (key == 'a') {
-        ship.setRotation(-0.1);
-    } else if (key == 'w') {
-        ship.boosting(true);
-
     }
 }
